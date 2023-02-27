8140f958cc8e536bc35bc56201ed8f283679f8ef
jailopmar <jaime96lopez@gmail.com>
2023-02-27 12:08:09 +0100

    Añadido dos interruptores, el primero controlara los leds1 y2 y el siguiente interruptor controlara el led3

--- code/code.ino
+++ code/code.ino
@@ -20,58 +20,115 @@ static const uint8_t FORCE_UPDATE_N_READS = 10;
 #define CHILD_ID 6
 #define ledPin2 7
 #define CHILD_ID2 7
+#define ledPin3 8
+#define CHILD_ID5 8
+
 #define CHILD_ID_HUM 0
 #define CHILD_ID_TEMP 1
 
+#define CHILD_ID31 31
+#define boton1 31
+#define boton2 33
+#define CHILD_ID33 33
 #define CHILD_ID3 2
 #define BUZZER_PIN 2
 
 #define CHILD_ID4 3
-#define BUTTON_PIN 3
+#define INF_PIN 3
 
 
 int oldValue = 0;
+int oldValue2 = 0;
 int buzzerState = HIGH;
 float lastTemp;
 float lastHum;
 uint8_t nNoUpdatesTemp;
 uint8_t nNoUpdatesHum;
 bool ledState = false;
+boolean lastButtonState = LOW;
+boolean lastButtonState2 = LOW;
 bool ledState2 = false;
+bool ledState3 = false;
+boolean buttonStateFromController = HIGH;
 
 MyMessage msg(CHILD_ID, V_LIGHT);
 MyMessage msg2(CHILD_ID2, V_LIGHT);
+MyMessage msg3(CHILD_ID5, V_LIGHT);
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 MyMessage msgBuzzer(CHILD_ID3, V_STATUS);
-MyMessage msgButton(CHILD_ID4, V_STATUS);
+MyMessage msgInf(CHILD_ID4, V_STATUS);
+MyMessage msgBoton1(CHILD_ID31, V_STATUS);
+MyMessage msgBoton2(CHILD_ID33, V_STATUS);
 
 DHT dht(DHT_DATA_PIN, DHTTYPE);
 
 void setup()
 {
   dht.begin();
+  pinMode(boton1, INPUT_PULLUP);
+  pinMode(boton2, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
 
   // Inicialización de MySensors
   sendSketchInfo("TFG Jaime Lopez Marquez", "0.1");
   present(CHILD_ID, S_LIGHT);
   present(CHILD_ID2, S_LIGHT);
+  present(CHILD_ID5, S_LIGHT);
   present(CHILD_ID_HUM, S_HUM);
   present(CHILD_ID_TEMP, S_TEMP);
   present(CHILD_ID3, S_BINARY);
   present(CHILD_ID4, S_DOOR);
+  present(CHILD_ID31, S_DOOR);
+  present(CHILD_ID33, S_DOOR);
+
 }
 
 void loop()
 {
 
-  int value = digitalRead(BUTTON_PIN);
+  boolean buttonState = digitalRead(boton1);
+
+  if (buttonState == LOW && lastButtonState == HIGH) {
+    //send(msgBoton1.set(!lastButtonState));
+    ledState = !ledState;
+    digitalWrite(ledPin, ledState);
+    digitalWrite(ledPin2, ledState);
+    send(msg.set(ledState));
+    send(msg2.set(ledState));
+    send(msgBoton1.set(ledState));
+
+    // Enviar el nuevo estado del LED al controlador Domoticz
+    
+  
+  }
+  lastButtonState = buttonState;
+
+  boolean buttonState2 = digitalRead(boton2);
+
+  if (buttonState2 == LOW && lastButtonState2 == HIGH) {
+    //send(msgBoton1.set(!lastButtonState));
+    ledState3 = !ledState3;
+    digitalWrite(ledPin3, ledState3);
+    send(msg3.set(ledState3));
+    send(msgBoton2.set(ledState3));
+
+    // Enviar el nuevo estado del LED al controlador Domoticz
+    
+  
+  }
+  lastButtonState2 = buttonState2;
+
+  
+
+  int value = digitalRead(INF_PIN);
+  //int valor = digitalRead(boton1);
 
   if (value != oldValue) {
-    send(msgButton.set(value == LOW ? 1 : 0));
+    send(msgInf.set(value == LOW ? 1 : 0));
     send(msgBuzzer.set(value == LOW ? 1 : 0));
     oldValue = value;
     buzzerState = !buzzerState;
@@ -100,6 +157,7 @@ void loop()
 
   
   // Comprobación del estado del LED
+    
   bool newState = digitalRead(ledPin);
   if (newState != ledState) {
     ledState = newState;
@@ -110,6 +168,12 @@ void loop()
   if (newState2 != ledState2) {
     ledState2 = newState2;
     send(msg2.set(ledState2));
+  }
+
+  bool newState3 = digitalRead(ledPin3);
+  if (newState3 != ledState3) {
+    ledState3 = newState3;
+    send(msg3.set(ledState3));
   }
 
   
@@ -127,16 +191,29 @@ void receive(const MyMessage &message) {
   }
 
   
-  if (message.getSensor()==CHILD_ID && message.getType()== V_STATUS) {
-     // Change relay state
+  if (message.getSensor()==CHILD_ID && message.type == V_STATUS) {
+    
     digitalWrite(ledPin, message.getBool()?HIGH:LOW);
-     // Store state in eeprom
-     Serial.print("Incoming change sensor:");
-     Serial.print(message.getSensor());
-     Serial.print(", New status: ");
-     Serial.println(message.getBool());
-     
-   } 
+  } 
+
+  if (message.getSensor()==CHILD_ID5 && message.type == V_STATUS) {
+    
+    digitalWrite(ledPin3, message.getBool()?HIGH:LOW);
+  } 
+  
+  if (message.getSensor()==CHILD_ID31 && message.type == V_STATUS) {
+    // Cambiar el estado del LED si se recibe un mensaje del botón
+    lastButtonState = message.getBool();
+    digitalWrite(ledPin, lastButtonState ? HIGH : LOW);
+    digitalWrite(ledPin2, lastButtonState ? HIGH : LOW);
+    //Serial.println(lastButtonState);
+  }
+
+  if (message.getSensor()==CHILD_ID33 && message.type == V_STATUS) {
+    // Cambiar el estado del LED si se recibe un mensaje del botón
+    lastButtonState = message.getBool();
+    digitalWrite(ledPin3, lastButtonState ? HIGH : LOW);
+  }
 
    if (message.getSensor()==CHILD_ID2 && message.getType()== V_STATUS) {
      // Change relay state
